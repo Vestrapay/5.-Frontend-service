0f83e243f4fc028ac73389b05d1838518373106f
Efeoghene Omonigho <omonigho.efeoghene@outlook.com>
2023-11-01 00:19:18 +0100

    Onboarding: URL query states🚀

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
 /*******/
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import SignIn from '@/components/onboarding/SignIn'
 import ChangePassword from '@/components/onboarding/ChangePassword'
 import SignUp from '@/components/onboarding/SignUp'
@@ -12,6 +8,7 @@ import CreatePassword from '@/components/onboarding/CreatePassword'
 import VerifySignUp from '@/components/onboarding/VerifySignUp'
 import VerifySignIn from '@/components/onboarding/VerifySignIn'
 import OnBoardingLayout from '@/components/layouts/OnBoardingLayout'
+import {useRouter} from "next/router";
 
 
 export default function Home() {
@@ -24,26 +21,49 @@ export default function Home() {
         phoneNumber: ""
     });
 
+    const router = useRouter()
+
+    useEffect(() => {
+        const sectionToPage: {[key: string]: string} = {
+            forgotpass: "forgotpass",
+            reset: "reset",
+            verifySignUp: "verifySignUp",
+            verifySignIn: "verifySignIn",
+            createpass: "createpass",
+            signin: "signin",
+            signup: "signup",
+        }
+
+        const {section} = router.query
+
+        const page = typeof section === "string" ? sectionToPage[section] : "signup"
+        setPage(page)
+    }, [router]);
+
+    const setPageFunc = (page: string) => {
+        router.push(`/login?section=${page}`).then(() => setPage(page))
+    }
+
     const renderPages = () => {
         switch (page) {
             case 'signin':
-                return <SignIn setPage={setPage} resetingPass={resetingPass} 
+                return <SignIn setPage={setPageFunc} resetingPass={resetingPass}
                 signInStatus={signInStatus} setResetingPass={setResetingPass} />;
             case 'forgotpass':
-                return <ChangePassword setPage={setPage} />;
+                return <ChangePassword setPage={setPageFunc} />;
             case 'reset':
-                return <ResetPassword setPage={setPage} setResetingPass={setResetingPass} />;
+                return <ResetPassword setPage={setPageFunc} setResetingPass={setResetingPass} />;
             case 'signup':
-                return <SignUp setPage={setPage} setPassData={setPassData}/>;
+                return <SignUp setPage={setPageFunc} setPassData={setPassData}/>;
             case 'createpass':
-                return <CreatePassword setPage={setPage} setResetingPass={setResetingPass} />;
+                return <CreatePassword setPage={setPageFunc} setResetingPass={setResetingPass} />;
             case 'verifySignUp':
-                return <VerifySignUp setPage={setPage} passData={passData} setSignInStatus={setSignInStatus} />;
+                return <VerifySignUp setPage={setPageFunc} passData={passData} setSignInStatus={setSignInStatus} />;
             case 'verifySignIn':
-                return <VerifySignIn setPage={setPage} />;
+                return <VerifySignIn setPage={setPageFunc} />;
 
             default:
-                return <SignUp setPage={setPage} />;
+                return <SignUp setPage={setPageFunc} />;
         }
     }
 
