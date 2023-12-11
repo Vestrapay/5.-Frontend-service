6c97c14cb680833387e56d2d2d808ee051f97267
Ayoola David <ayooladavid@gmail.com>
2023-12-11 07:06:49 +0100

    Update: Fix build error.

--- pages/settings/business/index.tsx
+++ pages/settings/business/index.tsx
@@ -5,23 +5,28 @@ import {SettingsNavProps} from "@types";
 export const businessNavLinks: SettingsNavProps[] = [
     {
         name: "About",
-        href: "/settings/business/about"
+        href: "/settings/business/about",
+        roles: ["USER"]
     },
     {
         name: "Settlement",
-        href: "/settings/business/settlement"
+        href: "/settings/business/settlement",
+        roles: ["USER"]
     },
     {
         name: "Notifications",
-        href: "/settings/business/notifications"
+        href: "/settings/business/notifications",
+        roles: ["USER"]
     },
     {
         name: "Security",
-        href: "/settings/business/security"
+        href: "/settings/business/security",
+        roles: ["USER"]
     },
     {
         name: "Payment Method",
-        href: "/settings/business/payment-method"
+        href: "/settings/business/payment-method",
+        roles: ["USER"]
     }
 ]
 
