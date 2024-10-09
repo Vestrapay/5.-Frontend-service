015f88d27f306aecc4d5fc7656743e0de703d9ab
dqve <ayooladavid@gmail.com>
2024-10-09 11:34:20 +0100

    Fix: Update fixes to issues on frontend.

--- Utils/URLs/index.ts
+++ Utils/URLs/index.ts
@@ -9,7 +9,7 @@ import { Storage } from "Utils/inAppstorage";
 
 
 const baseUrl = (): any => "http://3.76.247.111:8080/portal";//process.env.REACT_APP_BASE_URL;
-const paymentUrl= (): any => "http://3.76.247.111:8080/gateway";//process.env.REACT_APP_BASE_URL;
+const paymentUrl = (): any => "http://3.76.247.111:8080/gateway";//process.env.REACT_APP_BASE_URL;
 // const baseUrl = (): any => "https://dd52a73cb86e.ngrok.app";//process.env.REACT_APP_BASE_URL;
 // const paymentUrl= (): any => "https://e2edbdebbb2f.ngrok.app";//process.env.REACT_APP_BASE_URL;
 
@@ -48,18 +48,18 @@ export const apiCall = async ({ urlExtra, name, data = {}, params = {}, action =
             responseType: endPoints[theName] ? endPoints[theName]?.responseType || "json" : "json",
             data,
             params
-        })
+        }) 
             .then(async r => {
                 const returned = await action(r.data, r)
 
                 if ((r.data.statusCode === 201 || r.data.statusCode === 200 || r.status === 200 || r.status === 201 || r.status === 204) && !returned?.includes("skip")) {
                     successAlert(successDetails, r.data)
                     r?.data?.data ? res(r.data.data) : res(r.data)
-                } else if ((r.data.statusCode === "00" || r.status === 200 )|| (r.status === 201 || r.status !== 204)) {
-                    r?.data?.data ? res(r.data.data) : res(r.data)
                 }
-                else if (r.data.statusCode !== "00" && r.status !== 200 && r.status !== 201 && r.status !== 204) {
+                else if ((r.data.statusCode !== "00" && r.status !== 200) && (r.status !== 201 && r.status !== 204)) {
                     errorHandler(r)
+                } else if ((r.data.statusCode === "00" || r.status === 200) || (r.status === 201 || r.status !== 204)) {
+                    r?.data?.data ? res(r.data.data) : res(r.data)
                 } else if (returned?.includes("push")) {
                     successAlert(successDetails, r.data)
                 } else {
